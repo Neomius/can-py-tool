4584338545777b2e6d70a02234b4139dc7001b51
Göran Bakken <bakken.goran@gmail.com>
2017-01-04 20:56:07 +0100

    Second commit

---
+++ Src/main.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 4, 2017
+
+@author: goran
+'''
+
+def testfunction():
+    print "Hello World!"
+    
+
+testfunction()
+
+
+
+if __name__ == '__main__':
+    print "Runs as main!"
